39f5c3c6a1ec0b0fa79ba368f64f33fda50bc97a
David Morales <edavid.morales@udea.edu.co>
2023-01-24 21:24:58 -0500

    Se agregan los servicios de facturas y usuarios

--- src/main/java/com/example/parqueadero/servicios/FacturaServices.java
+++ src/main/java/com/example/parqueadero/servicios/FacturaServices.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -23,5 +24,57 @@ public class FacturaServices {
 
     public Optional<Factura> obtenerFacturaXId(int id){
         return this.facturaRepository.findById(id);
+    }
+
+    public void eliminarFactura(int id){
+        this.facturaRepository.deleteById(id);
+    }
+    public Factura crearFactura(Factura nuevaFactura){
+        return this.facturaRepository.save(nuevaFactura);
+    }
+
+    public Factura editarFactura(Factura factura, int id){
+        Factura factura1 = facturaRepository.findById(id).get();
+
+        if(Objects.nonNull(
+                factura.getNumeroFactura())
+            && !"".equalsIgnoreCase(
+                    factura.getNumeroFactura().toString())) {
+            factura1.setNumeroFactura(
+                    factura.getNumeroFactura());
+        }
+        if (Objects.nonNull(
+                factura.getUsuarioid())
+                && !"".equalsIgnoreCase(
+                factura.getUsuarioid().toString())) {
+            factura1.setUsuarioid(
+                    factura.getUsuarioid());
+        }
+        if (Objects.nonNull(
+                factura.getCarroid())
+                        && !"".equalsIgnoreCase(
+                        factura.getCarroid().toString())) {
+            factura1.setCarroid(
+                    factura.getCarroid());
+        }
+        if(Objects.nonNull(
+                factura.getDescuento())
+     /*   && !"".equalsIgnoreCase(
+                factura.getDescuento())*/){
+            factura1.setDescuento(
+                    factura.getDescuento());
+        }
+        if(Objects.nonNull(
+                factura.getHoraParqueo())){
+            factura1.setHoraParqueo(
+                    factura.getHoraParqueo());
+        }
+        if(Objects.nonNull(
+                factura.getTotalPagar())){
+            factura1.setTotalPagar(
+                    factura.getTotalPagar());
+        }
+
+        return this.facturaRepository.save(factura1);
     }
 }
--- src/main/java/com/example/parqueadero/servicios/UsuarioServices.java
+++ src/main/java/com/example/parqueadero/servicios/UsuarioServices.java
@@ -1,8 +1,13 @@
 package com.example.parqueadero.servicios;
 
+import com.example.parqueadero.entidades.Usuario;
 import com.example.parqueadero.repositorios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class UsuarioServices {
@@ -12,4 +17,76 @@ public class UsuarioServices {
     public UsuarioServices(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+
+    public List<Usuario> obtenerUsuarios(){
+        return this.usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> obtenerUsuarioXId(int id){
+        return this.usuarioRepository.findById(id);
+    }
+
+    public Usuario crearUsuario(Usuario nuevoUsuario){
+        return this.usuarioRepository.save(nuevoUsuario);
+    }
+    public void eliminarUsuario(int id){
+        this.usuarioRepository.deleteById(id);
+    }
+
+    public Usuario editarUsuario(Usuario usuario, int id){
+        Usuario usuario1 = usuarioRepository.findById(id).get();
+
+        if(Objects.nonNull(
+                usuario.getApellido())
+        && !"".equalsIgnoreCase(
+                usuario.getApellido().toString())){
+            usuario1.setApellido(
+                    usuario.getApellido());
+        }
+        if(Objects.nonNull(
+                usuario.getCarroid())
+        && !"".equalsIgnoreCase(
+                usuario.getCarroid().toString())){
+            usuario1.setCarroid(
+                    usuario.getCarroid());
+        }
+        if(Objects.nonNull(
+                usuario.getFacturaid())
+        && !"".equalsIgnoreCase(
+                usuario.getFacturaid().toString())){
+            usuario1.setFacturaid(
+                    usuario.getFacturaid());
+        }
+        if(Objects.nonNull(
+                usuario.getCedula())
+        && !"".equalsIgnoreCase(
+                usuario.getCedula().toString())){
+            usuario1.setCedula(
+                    usuario.getCedula());
+        }
+        if(Objects.nonNull(
+                usuario.getCelular())
+        && !"".equalsIgnoreCase(
+                usuario.getCelular().toString())){
+            usuario1.setCelular(
+                    usuario.getCelular());
+        }
+        if(Objects.nonNull(
+                usuario.getCorreo())
+        && !"".equalsIgnoreCase(
+                usuario.getCorreo().toString())){
+            usuario1.setCorreo(
+                    usuario.getCorreo());
+        }
+        if(Objects.nonNull(
+                usuario.getNombre())
+        && !"".equalsIgnoreCase(
+                usuario.getNombre().toString())){
+            usuario1.setNombre(
+                    usuario.getNombre());
+        }
+        
+        return this.usuarioRepository.save(usuario1);
+    }
+
 }
